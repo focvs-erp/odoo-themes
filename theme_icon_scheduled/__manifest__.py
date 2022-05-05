76edd13f836dc16d7d6e88d9b0512d754f987562
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-05-05 11:59:02 -0400

    adicionado novo modulo que faz a alteração dos icones de atividades agendadas

---
+++ theme_icon_scheduled/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Theme Icons Scheduled",
+    "Description": "Aplicativo responsável por alterar os icones de agendamento de atividade.",
+    "depends": [
+        'base', 
+        'web_enterprise', 
+        'web', 
+        'mail'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    "sequence": 1,
+}
